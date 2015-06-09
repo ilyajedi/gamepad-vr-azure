a48b2f629d5dc778e323d5629ea813b30ccc0c41
ilyajedi <ilyajedi@ya.ru>
2015-06-09 17:34:08 +0300

    Basic UI created

---
+++ gamepad-vr-azure/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(gamepad_vr_azure.Startup))]
+
+namespace gamepad_vr_azure
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
